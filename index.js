f5e2088fa89fcba8948227c86822643a70ea385e
Arun Ganesh <arun.planemad@gmail.com>
2015-06-10 14:29:12 -0400

    Loop through linestring and calculate distance between consecutive points

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-var wgs84 = require('wgs84');
+var haversine = require('haversine');
+
+module.exports.filter = filter;
+
+// Filter the input geojson 
+function filter(geojson) {
+
+    var lineString = geojson.features[0].geometry.coordinates;
+
+    // Loop through the coordinate array of the line to determine distance between consecutive points
+    for (var i = 0; i < lineString.length-1; i++) {
+
+        var point1 = {
+            latitude: lineString[i][1],
+            longitude: lineString[i][0]
+        }
+        var point2 = {
+            latitude: lineString[i+1][1],
+            longitude: lineString[i+1][0]
+        }
+        
+        // Calculate haversine distance between 2 points in metres
+        console.log(haversine(point1, point2, {unit: 'km'}) * 1000);
+    }
+
+    return true;
+
+}
