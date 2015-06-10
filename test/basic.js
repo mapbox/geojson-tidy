e4551c2be8a57bfbb8213f48c321aa79adf36696
Arun Ganesh <arun.planemad@gmail.com>
2015-06-10 17:21:26 -0400

    Update comments

--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,21 @@
 var geojsonTidy = require('../'),
     test = require('tape'),
     walk1Json = require('./walk-1.json'),
-    walk2Json = require('./walk-2.json');
+    walk2Json = require('./walk-2.json'); // has no timestamps
 
 test('geojson tidy', function(t) {
     
-    t.test('Walk 1', function(t) {
+//    t.test('Output not equal to input', function(t) {
+//        t.notEqual(geojsonTidy.tidy(walk1Json), true);
+//        t.end();
+//    });
+    
+    t.test('Tidy walk with timestamps', function(t) {
         t.equal(geojsonTidy.tidy(walk1Json), true);
         t.end();
     });
     
-    t.test('Walk 2', function(t) {
+    t.test('Tidy walk without timestamps', function(t) {
         t.equal(geojsonTidy.tidy(walk2Json), true);
         t.end();
     });
