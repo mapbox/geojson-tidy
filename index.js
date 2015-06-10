6eefffb5c33937971f7bd3efa7bda473f9276868
Arun Ganesh <arun.planemad@gmail.com>
2015-06-10 15:04:09 -0400

    Add extend for working with default options

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-var haversine = require('haversine');
+var extend = require('extend'),
+    haversine = require('haversine');
 
 module.exports.filter = filter;
 
 // Filter the input geojson 
-function filter(geojson) {
+function filter(geojson, options) {
 
-    var lineString = geojson.features[0].geometry.coordinates;
+    var lineString = geojson.features[0].geometry.coordinates,
+        tidyLineString = [];
+
+    // Set the minimum distance in metres and displacement in 10 seconds between successive coordinates
+    var defaults = {
+        minDistance : 7 ,
+    }
+    var filter = extend(defaults, options);
 
     // Loop through the coordinate array of the line to determine distance between consecutive points
-    for (var i = 0; i < lineString.length-1; i++) {
+    for (var i = 0; i < lineString.length - 1; i++) {
 
         var point1 = {
             latitude: lineString[i][1],
             longitude: lineString[i][0]
         }
         var point2 = {
-            latitude: lineString[i+1][1],
-            longitude: lineString[i+1][0]
+            latitude: lineString[i + 1][1],
+            longitude: lineString[i + 1][0]
         }
-        
+
         // Calculate haversine distance between 2 points in metres
-        console.log(haversine(point1, point2, {unit: 'km'}) * 1000);
+        distance12 = haversine(point1, point2, {
+            unit: 'km'
+        }) * 1000;
+
+//        console.log(distance12);
+
+        // 
+        if (distance12 > filter.minDistance) {
+            tidyLineString.push(lineString[i]);
+        }
+
     }
+
+    // Tidy output
+    console.log("Input points: " + lineString.length + "\nOutput points: " + tidyLineString.length+"\n");
 
     return true;
 
--- test/basic.js
+++ test/basic.js
@@ -3,9 +3,21 @@ var geojsonTidy = require('../'),
     walkingJson = require('./walking-route.json');
 
 test('geojson tidy', function(t) {
-    t.test('checks if the function works', function(t) {
+    
+    t.test('Defult 7m filter', function(t) {
         t.equal(geojsonTidy.filter(walkingJson), true);
         t.end();
     });
+    
+    t.test('5m filter', function(t) {
+        t.equal(geojsonTidy.filter(walkingJson, {minDistance : 5}), true);
+        t.end();
+    });
+    
+    t.test('20m filter', function(t) {
+        t.equal(geojsonTidy.filter(walkingJson, {minDistance : 20}), true);
+        t.end();
+    });
+    
     t.end();
 });
